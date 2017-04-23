059e30f566300176e640c682669a730ba4d57387
playra <raoffonom@icloud.com>
2017-04-23 03:08:01 +0300

    first commit

---
+++ appiSoulClub /app/src/main/java/ru/clevercon/appisoulclub/MainActivity.java
@@ -0,0 +1,19 @@
+package ru.clevercon.appisoulclub;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        String url = "http://isoulclub.ru";
+        WebView view = (WebView) this.findViewById(R.id.webView);
+        view.getSettings().setJavaScriptEnabled(true);
+        view.loadUrl(url);
+    }
+}
